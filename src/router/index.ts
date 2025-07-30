3af12f9cff49669171f402e4469e0ab4101b83c8
nhy <niehengyang@ebupt.com>
2025-07-31 02:11:36 +0800

    feat： 修改路由模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 import List from '@/views/stations/List.vue';
 import Index from '@/views/home/Index.vue';
 
@@ -16,7 +16,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
