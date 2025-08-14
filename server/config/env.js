fce66a7c744db4a7a92e36eb99105b18e4432da2
niehengyang <790227542@qq.com>
2025-08-14 14:31:30 +0800

    feat: 地图新增搜索功能

--- server/config/env.js
+++ server/config/env.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 // 环境变量配置
 export const PORT = process.env.PORT || 3001;
-export const DATA_DIR = path.resolve(process.env.DATA_DIR || '/data');
+export const DATA_DIR = path.resolve(process.env.DATA_DIR || './data');
 export const DB_FILE = process.env.DB_FILE || 'db.json';
 export const DB_PATH = path.join(DATA_DIR, DB_FILE);
 export const PICTURES_DIR = process.env.PICTURES_DIR || '/pictures';
